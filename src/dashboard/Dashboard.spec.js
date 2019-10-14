47e75d8f6e993ffb376a76ed6f8c4c005f40ea60
James Grantham <james.r.grantham@gmail.com>
2019-10-14 13:33:43 +0100

    Create 4 basic checks, require refining

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,34 @@
 // Test away
+
+import React from 'react';
+import * as rtl from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Dashboard from './Dashboard';
+
+afterEach(rtl.cleanup);
+
+let wrapper;
+
+beforeEach(() =>{
+  wrapper = rtl.render(<Dashboard />);
+})
+
+describe('Dashboard functionality', () =>{
+
+  test('gate starts open', () => {
+    expect(wrapper.queryByText(/open/i)).toBeInTheDocument();
+  });
+
+  test('button to close gate', () => {
+    expect(wrapper.queryByText(/close gate/i)).toBeInTheDocument();
+  })
+
+  test('gate starts unlocked', () => {
+    expect(wrapper.queryByText(/unlocked/i)).toBeInTheDocument();
+  })
+
+  test('button to lock gate', () => {
+    expect(wrapper.queryByText(/lock gate/i)).toBeInTheDocument();
+  })
+
+})
